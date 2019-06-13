c309b50d2e2013389cd7e6b4a072e5e88ebef7f5
Steve C <git@trumpton.org.uk>
2019-06-13 22:56:08 +0100

    Fixed invalid pointer when model() not defined

--- safelistview.cpp
+++ safelistview.cpp
@@ -6,18 +6,19 @@ SafeListView::SafeListView(QWidget *parent) :
 }
 
 void SafeListView::focusInEvent(QFocusEvent * e)
-{
+{    
     QListView::focusInEvent(e) ;
     QModelIndex idx = currentIndex() ;
 
-    if (!idx.isValid()) {
+    if (!idx.isValid() && model()) {
         idx = model()->index(0, 0) ;
     }
 
-    //setCurrentIndex(idx) ;
-    selectionModel()->select(idx, QItemSelectionModel::Select);
-    int i = idx.row() ;
-
-    emit gainedFocus("Selected Row " + QString::number(i) + ". ") ;
+    if (idx.isValid()) {
+        //setCurrentIndex(idx) ;
+        selectionModel()->select(idx, QItemSelectionModel::Select);
+        int i = idx.row() ;
+        emit gainedFocus("Selected Row " + QString::number(i) + ". ") ;
+    }
 
 }
