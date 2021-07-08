ff8a7e353ef7eba00009e73e67c28a6b85e13d25
Steve Clarke <git@trumpton.org.uk>
2021-07-08 20:55:08 +0100

    Move to QSoundEffect

--- alertsound.cpp
+++ alertsound.cpp
@@ -1,44 +1,46 @@
 #include "alertsound.h"
 
-#include <QSound>
+#include <QSoundEffect>
+#include <QUrl>
+
 #include <QApplication>
 #include <QDir>
 
-QSound *sound_ok ;
-QSound *sound_alert ;
-QSound *sound_error ;
-QSound *sound_query ;
+QSoundEffect sound_ok ;
+QSoundEffect sound_alert ;
+QSoundEffect sound_error ;
+QSoundEffect sound_query ;
 
-QSound *sound_disabled ;
+QSoundEffect sound_disabled ;
 
-QSound *sound_notfound ;
-QSound *sound_found ;
-QSound *sound_wrapped ;
+QSoundEffect sound_notfound ;
+QSoundEffect sound_found ;
+QSoundEffect sound_wrapped ;
 
-QSound *sound_fileopen ;
-QSound *sound_filesave ;
+QSoundEffect sound_fileopen ;
+QSoundEffect sound_filesave ;
 
-QSound *sound_sent ;
+QSoundEffect sound_sent ;
 
 static int sound_initialised = 0 ;
 
 void initSound()
 {
-    sound_ok = new QSound(":/sounds/ok.wav") ;
-    sound_alert = new QSound(":/sounds/alert.wav") ;
-    sound_query = new QSound(":/sounds/query.wav") ;
-    sound_error = new QSound(":/sounds/error.wav") ;
+    sound_ok.setSource(QUrl("qrc:/sounds/ok.wav")) ;
+    sound_alert.setSource(QUrl("qrc:/sounds/alert.wav")) ;
+    sound_query.setSource(QUrl("qrc:/sounds/query.wav")) ;
+    sound_error.setSource(QUrl("qrc:/sounds/error.wav")) ;
 
-    sound_disabled = new QSound(":/sounds/disabled.wav") ;
+    sound_disabled.setSource(QUrl("qrc:/sounds/disabled.wav")) ;
 
-    sound_found = new QSound(":/sounds/found.wav") ;
-    sound_notfound = new QSound(":/sounds/notfound.wav") ;
-    sound_wrapped = new QSound(":/sounds/wrapped.wav") ;
+    sound_found.setSource(QUrl("qrc:/sounds/found.wav")) ;
+    sound_notfound.setSource(QUrl("qrc:/sounds/notfound.wav")) ;
+    sound_wrapped.setSource(QUrl("qrc:/sounds/wrapped.wav")) ;
 
-    sound_fileopen = new QSound(":/sounds/fileopen.wav") ;
-    sound_filesave = new QSound(":/sounds/filesave.wav") ;
+    sound_fileopen.setSource(QUrl("qrc:/sounds/fileopen.wav")) ;
+    sound_filesave.setSource(QUrl("qrc:/sounds/filesave.wav")) ;
 
-    sound_sent = new QSound(":/sounds/sent.wav") ;
+    sound_sent.setSource(QUrl("qrc:/sounds/sent.wav")) ;
 
     sound_initialised = 1 ;
 }
@@ -47,38 +49,30 @@ void play(enum soundType type)
 {
     if (sound_initialised==0) initSound() ;
 
-    QSound *sound ;
+    QSoundEffect *sound ;
     switch (type) {
-    case Alert: sound = sound_alert ; break ;
-    case Error: sound = sound_error ; break ;
-    case Query: sound = sound_query ; break ;
-    case Disabled: sound = sound_disabled ; break ;
-    case Found: sound = sound_found ; break ;
-    case NotFound: sound = sound_notfound ; break ;
-    case Wrapped: sound = sound_wrapped ; break ;
-    case FileOpen: sound = sound_fileopen ; break ;
-    case FileSave: sound = sound_filesave ; break ;
-    case Sent: sound = sound_sent ; break ;
+    case Alert: sound = &sound_alert ; break ;
+    case Error: sound = &sound_error ; break ;
+    case Query: sound = &sound_query ; break ;
+    case Disabled: sound = &sound_disabled ; break ;
+    case Found: sound = &sound_found ; break ;
+    case NotFound: sound = &sound_notfound ; break ;
+    case Wrapped: sound = &sound_wrapped ; break ;
+    case FileOpen: sound = &sound_fileopen ; break ;
+    case FileSave: sound = &sound_filesave ; break ;
+    case Sent: sound = &sound_sent ; break ;
     default:
     case Ok:
-        sound = sound_ok ; break ;
+        sound = &sound_ok ; break ;
     }
+    QSoundEffect::Status status1 = sound->status() ;
+    sound->setVolume(1) ;
+    sound->setMuted(false) ;
     sound->play() ;
+    QSoundEffect::Status status2 = sound->status() ;
+    return ;
 }
 
 void closeSound()
 {
-    if (sound_initialised!=0) {
-        if (sound_alert) delete sound_alert ;
-        if (sound_error) delete sound_error ;
-        if (sound_query) delete sound_query ;
-        if (sound_disabled) delete sound_disabled ;
-        if (sound_found) delete sound_found ;
-        if (sound_notfound) delete sound_notfound ;
-        if (sound_wrapped) delete sound_wrapped ;
-        if (sound_fileopen) delete sound_fileopen ;
-        if (sound_filesave) delete sound_filesave ;
-        if (sound_sent) delete sound_sent ;
-        sound_initialised=0 ;
-    }
 }
