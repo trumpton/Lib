b89e8bffe3ccb5948625f73ba1c242e746af2441
Steve Clarke <git@trumpton.org.uk>
2021-07-08 22:13:15 +0100

    alertsound accepts parent qobject

--- alertsound.cpp
+++ alertsound.cpp
@@ -6,41 +6,58 @@
 #include <QApplication>
 #include <QDir>
 
-QSoundEffect sound_ok ;
-QSoundEffect sound_alert ;
-QSoundEffect sound_error ;
-QSoundEffect sound_query ;
+QSoundEffect *sound_ok ;
+QSoundEffect *sound_alert ;
+QSoundEffect *sound_error ;
+QSoundEffect *sound_query ;
 
-QSoundEffect sound_disabled ;
+QSoundEffect *sound_disabled ;
 
-QSoundEffect sound_notfound ;
-QSoundEffect sound_found ;
-QSoundEffect sound_wrapped ;
+QSoundEffect *sound_notfound ;
+QSoundEffect *sound_found ;
+QSoundEffect *sound_wrapped ;
 
-QSoundEffect sound_fileopen ;
-QSoundEffect sound_filesave ;
+QSoundEffect *sound_fileopen ;
+QSoundEffect *sound_filesave ;
 
-QSoundEffect sound_sent ;
+QSoundEffect *sound_sent ;
 
 static int sound_initialised = 0 ;
 
-void initSound()
+void initSound(QObject *parent = nullptr)
 {
-    sound_ok.setSource(QUrl("qrc:/sounds/ok.wav")) ;
-    sound_alert.setSource(QUrl("qrc:/sounds/alert.wav")) ;
-    sound_query.setSource(QUrl("qrc:/sounds/query.wav")) ;
-    sound_error.setSource(QUrl("qrc:/sounds/error.wav")) ;
+    sound_ok = new QSoundEffect(parent) ;
+    sound_ok->setSource(QUrl("qrc:/sounds/ok.wav")) ;
 
-    sound_disabled.setSource(QUrl("qrc:/sounds/disabled.wav")) ;
+    sound_alert = new QSoundEffect(parent) ;
+    sound_alert->setSource(QUrl("qrc:/sounds/alert.wav")) ;
 
-    sound_found.setSource(QUrl("qrc:/sounds/found.wav")) ;
-    sound_notfound.setSource(QUrl("qrc:/sounds/notfound.wav")) ;
-    sound_wrapped.setSource(QUrl("qrc:/sounds/wrapped.wav")) ;
+    sound_query = new QSoundEffect(parent) ;
+    sound_query->setSource(QUrl("qrc:/sounds/query.wav")) ;
 
-    sound_fileopen.setSource(QUrl("qrc:/sounds/fileopen.wav")) ;
-    sound_filesave.setSource(QUrl("qrc:/sounds/filesave.wav")) ;
+    sound_error = new QSoundEffect(parent) ;
+    sound_error->setSource(QUrl("qrc:/sounds/error.wav")) ;
 
-    sound_sent.setSource(QUrl("qrc:/sounds/sent.wav")) ;
+    sound_disabled = new QSoundEffect(parent) ;
+    sound_disabled->setSource(QUrl("qrc:/sounds/disabled.wav")) ;
+
+    sound_found = new QSoundEffect(parent) ;
+    sound_found->setSource(QUrl("qrc:/sounds/found.wav")) ;
+
+    sound_notfound = new QSoundEffect(parent) ;
+    sound_notfound->setSource(QUrl("qrc:/sounds/notfound.wav")) ;
+
+    sound_wrapped = new QSoundEffect(parent) ;
+    sound_wrapped->setSource(QUrl("qrc:/sounds/wrapped.wav")) ;
+
+    sound_fileopen = new QSoundEffect(parent) ;
+    sound_fileopen->setSource(QUrl("qrc:/sounds/fileopen.wav")) ;
+
+    sound_filesave = new QSoundEffect(parent) ;
+    sound_filesave->setSource(QUrl("qrc:/sounds/filesave.wav")) ;
+
+    sound_sent = new QSoundEffect(parent) ;
+    sound_sent->setSource(QUrl("qrc:/sounds/sent.wav")) ;
 
     sound_initialised = 1 ;
 }
@@ -51,21 +68,20 @@ void play(enum soundType type)
 
     QSoundEffect *sound ;
     switch (type) {
-    case Alert: sound = &sound_alert ; break ;
-    case Error: sound = &sound_error ; break ;
-    case Query: sound = &sound_query ; break ;
-    case Disabled: sound = &sound_disabled ; break ;
-    case Found: sound = &sound_found ; break ;
-    case NotFound: sound = &sound_notfound ; break ;
-    case Wrapped: sound = &sound_wrapped ; break ;
-    case FileOpen: sound = &sound_fileopen ; break ;
-    case FileSave: sound = &sound_filesave ; break ;
-    case Sent: sound = &sound_sent ; break ;
+    case Alert: sound = sound_alert ; break ;
+    case Error: sound = sound_error ; break ;
+    case Query: sound = sound_query ; break ;
+    case Disabled: sound = sound_disabled ; break ;
+    case Found: sound = sound_found ; break ;
+    case NotFound: sound = sound_notfound ; break ;
+    case Wrapped: sound = sound_wrapped ; break ;
+    case FileOpen: sound = sound_fileopen ; break ;
+    case FileSave: sound = sound_filesave ; break ;
+    case Sent: sound = sound_sent ; break ;
     default:
     case Ok:
-        sound = &sound_ok ; break ;
+        sound = sound_ok ; break ;
     }
-    QSoundEffect::Status status1 = sound->status() ;
     sound->setVolume(1) ;
     sound->setMuted(false) ;
     sound->play() ;
@@ -75,4 +91,17 @@ void play(enum soundType type)
 
 void closeSound()
 {
+  if (sound_initialised==1) {
+      delete sound_alert ;
+      delete sound_error ;
+      delete sound_query ;
+      delete sound_disabled ;
+      delete sound_found ;
+      delete sound_notfound ;
+      delete sound_wrapped ;
+      delete sound_fileopen ;
+      delete sound_filesave ;
+      delete sound_sent ;
+      sound_initialised=0 ;
+  }
 }
--- alertsound.h
+++ alertsound.h
@@ -1,11 +1,13 @@
 #ifndef ALERTSOUND_H
 #define ALERTSOUND_H
 
+#include <QObject>
+
 enum soundType {
     Ok, Alert, Error, Query, Disabled, Found, NotFound, Wrapped, FileOpen, FileSave, Sent
 };
 
-void initSound() ;
+void initSound(QObject *parent) ;
 void closeSound() ;
 void play(enum soundType type) ;
 
